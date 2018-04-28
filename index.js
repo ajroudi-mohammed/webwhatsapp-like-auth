271202c9425d658bb69803599c01b29d193f8290
mohammmed <mohammed.aj@comenscene.com>
2018-04-28 01:03:03 +0100

    installation du QR code generator + premiere connexion socket yay !

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require("express");
 var socket = require("socket.io");
+var qr = require("qr-image");
 
 //App setup
 var app = express();
+
+/*app.get("/", function(req, res){
+	var code = qr.image("this is my freaking text", {type : 'png'});
+
+	res.type("png");
+	
+	code.pipe(res);
+});*/
+
+
 var server = app.listen(4000, function(){
 	console.log("listening to requests on port 4000"); 
 });
+
 
 // Static files
 app.use(express.static("public"));
@@ -15,5 +27,8 @@ var io = socket(server);
 
 io.on('connection', function(socket){
 	console.log('made a socket connection : '+socket.id);
-
+	socket.on('join', function(data) {
+		console.log(data);
+		io.emit('messages', 'Hello');
+	});
 });
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,9 @@
 // Make connection
 var socket = io.connect("http://localhost:4000");
+
+socket.on('connect', function(data) {
+    socket.emit('join', 'Hello World from client');
+});
+socket.on('messages', function(data) {
+    alert(data);
+});
